c98146ed938e55ff62876c06982bbb8a0c113491
td <thaisduog07@gmail.com>
2025-06-12 16:15:19 +0700

    initial setup

---
+++ backend/hethongthongtin/src/main/java/com/example/hethongthongtin/HethongthongtinApplication.java
@@ -0,0 +1,13 @@
+package com.example.hethongthongtin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HethongthongtinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HethongthongtinApplication.class, args);
+	}
+
+}
---
+++ backend/hethongthongtin/src/test/java/com/example/hethongthongtin/HethongthongtinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hethongthongtin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HethongthongtinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
