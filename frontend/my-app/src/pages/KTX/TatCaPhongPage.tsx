5e234bd225bf5a7f5a0f9f69a7740695ad815889
td <thaisduog07@gmail.com>
2025-07-12 23:41:05 +0700

    xoa file rac

--- frontend/my-app/src/pages/KTX/TatCaPhongPage.tsx
+++ frontend/my-app/src/pages/KTX/TatCaPhongPage.tsx
@@ -2,7 +2,6 @@ export const TatCaPhongPage = () => {
   return (
     <div>
       <h1>Tất cả phòng</h1>
-      {/* Nội dung của trang Tất cả phòng sẽ được thêm vào đây */}
     </div>
   );
 };
--- frontend/my-app/src/pages/KTX/TheoDoiVaPhongCuaToiPage.tsx
+++ frontend/my-app/src/pages/KTX/TheoDoiVaPhongCuaToiPage.tsx
@@ -2,7 +2,6 @@ export const TheoDoiVaPhongCuaToiPage = () => {
   return (
     <div>
       <h1>Theo dõi và phòng của tôi</h1>
-      {/* Nội dung của trang Theo dõi và phòng của tôi sẽ được thêm vào đây */}
     </div>
   );
 };
